94c22850d49a0cc61bd48b30310f9dc3b25c3fef
NguyenDucTuanUET <64391055+ductuanUET@users.noreply.github.com>
2023-06-28 16:32:52 +0700

    update front-end: home page with carousel and navbar

--- backend/src/main/java/dev/tuannd/movies/MoviesApplication.java
+++ backend/src/main/java/dev/tuannd/movies/MoviesApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class MoviesApplication {
 
 	public static void main(String[] args) {
